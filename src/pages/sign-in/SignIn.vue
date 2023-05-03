a3cc86af941ef38054c563e8b404a4b91297899c
Zhor <zhor@ThinkPad-L14-Gen-2-d7727f99.localdomain>
2023-05-03 14:55:55 +0300

    initial commit

---
+++ src/pages/sign-in/SignIn.vue
@@ -0,0 +1,26 @@
+<script setup>
+import TopNavbar from '../../components/TopNavbar.vue';
+</script>
+
+<template>
+    <TopNavbar />
+</template>
+<style></style>
+
+<script>
+export default {
+    components: {
+        SignIn
+    },
+
+    computed: {},
+
+    mounted() {
+    },
+
+    data() {
+        return {
+        };
+    },
+};
+</script>
